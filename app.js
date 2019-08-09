5d983dbcb8a65b1aabb689687f23f1885028ac10
Simonas Mulevicius <mulevicius.simonas@gmail.com>
2019-08-09 18:54:39 +0300

    Major functionality update
    Fixed filtering functionality which previously was not working at all

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const xlsx = require('node-xlsx');
 
 const formatting = require('./util/formatting');
-var config = require('./config');
+let config = require('./config');
 
 const app = express();
 
@@ -17,124 +17,92 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/filterapp', (req, res, next) => {
-    res.render('filterapp');
+//-----------------For testing only---------------+
+formatting.runAllTests();
+//------------------------------------------------+
+
+app.get('/', (req, res, next) => {
+    res.render('home', { title: 'Welcome to Fasterliq' });
 });
 
-app.post('/fileuploaded', (req, res) => {
-    var form = new formidable.IncomingForm();
-    form.parse(req, (err, fields, files) => {
-        //1. Store files in memory
-        var clientsFileName = files['clientsfile'].name;
-        var unwantedClientsFileName = files['unwantedclientsfile'].name;
+app.get('/filterapp', (req, res, next) => {
+    res.render('filterapp', {
+        title: 'Filter Clients',
+        firstTime: true,
+        foundErrors: false,
+        responseMessage: []
+    });
+});
 
-        console.log("Clientsfile name is:" + clientsFileName);
-        console.log("Unwantedclientsfile name is:" + unwantedClientsFileName);
+app.post('/filterapp', (req, res, next) => {
+    let form = new formidable.IncomingForm();
+    form.parse(req, (err, fields, files) => {
+        let clientsFileName = files['clientsfile'].name;
+        let unwantedClientsFileName = files['unwantedclientsfile'].name;
 
         if (!(clientsFileName.endsWith('.xlsx') ||
                 clientsFileName.endsWith('.xls') ||
                 unwantedClientsFileName.endsWith('.xlsx') ||
                 unwantedClientsFileName.endsWith('.xls')
             )) {
-            res.redirect('/invalidFile');
-            return;
+            return res.redirect('/invalidFile');
         }
+        console.log("clientsFileName is:" + clientsFileName);
+        console.log("unwantedClientsFileName is:" + unwantedClientsFileName);
 
         const timePrefix = formatting.getTimePrefix();
 
-        var oldClientsFilePath = files['clientsfile'].path;
-        var newClientsFilePath = __dirname + '\\uploads' + timePrefix + clientsFileName;
+        let oldClientsFilePath = files['clientsfile'].path;
+        let newClientsFilePath = __dirname + '\\uploads' + timePrefix + clientsFileName;
         fs.rename(oldClientsFilePath, newClientsFilePath, (err) => {
             if (err) console.log(err);
 
-            var oldUnwantedClientsFilePath = files['unwantedclientsfile'].path;
-            var newUnwantedClientsFilePath = __dirname + '\\uploads' + timePrefix + unwantedClientsFileName;
+            let oldUnwantedClientsFilePath = files['unwantedclientsfile'].path;
+            let newUnwantedClientsFilePath = __dirname + '\\uploads' + timePrefix + unwantedClientsFileName;
             fs.rename(oldUnwantedClientsFilePath, newUnwantedClientsFilePath, (err) => {
                 if (err) console.log(err);
 
-                // //2. Parse files
-                var clientsXlsx = xlsx.parse(newClientsFilePath);
+                //1. Upload information from files
+                let allClients = formatting.getAllClients(newClientsFilePath);
+                let unWantedPhoneNumbers = formatting.getUnwantedPhoneNumbers(newUnwantedClientsFilePath);
+                let logger = {};
+                logger.responseMessage = [];
+                logger.foundErrors = false;
 
-                //Unwanted clients:
-                var unWantedPhoneNumbers = formatting.getUnwantedPhoneNumbers(newUnwantedClientsFilePath);
-                console.log("List of unwanted phone numbers:");
-                for (var i = 0; i < unWantedPhoneNumbers.length; i++) {
-                    console.log(unWantedPhoneNumbers[i]);
-                }
-                console.log("THE END of the list of unwanted phone numbers:");
-                console.log(unWantedPhoneNumbers);
+                //2. Filter contacts
+                let filteredClients = formatting.filterContacts(allClients, unWantedPhoneNumbers, logger);
 
-                var phoneNumberRows = [];
-                //looping through all sheets
-                for (var i = 0; i < clientsXlsx.length; i++) {
-                    var sheet = clientsXlsx[i];
-                    //loop through all rows in the sheet
-                    for (var j = 0; j < sheet['data'].length; j++) {
-                        //add the row to the rows array
-                        if (sheet['data'][j].length >= 2) {
-                            var pair = [];
-                            pair.push(String(sheet['data'][j][0]));
-                            pair.push(String(sheet['data'][j][1]));
-                            phoneNumberRows.push(pair);
-                        }
-                    }
-                }
-                console.log(phoneNumberRows);
-
-                //create a set of good phone numbers
-                var writeStr = "";
-
-                let phoneData = [];
-
-                let errorsFound = '<ol>';
-                console.log("List of parsed phone numbers");
-                for (var i = 0; i < phoneNumberRows.length; i++) {
-                    if (phoneNumberRows[i].length >= 2) {
-                        if (!unWantedPhoneNumbers.includes(phoneNumberRows[i][1])) {
-                            let initialName = phoneNumberRows[i][0];
-                            let transformedName = formatting.transformName(phoneNumberRows[i][0]);
-                            let phoneNumber = phoneNumberRows[i][1];
-                            let newGoodClient = [initialName, transformedName, phoneNumber];
-                            phoneData.push(newGoodClient);
-                            //console.log("CORRECT: " + text); 
-                        } else {
-                            //Unwanted phone number is detected
-                            let text = phoneNumberRows[i][0] + ', ' + phoneNumberRows[i][1];
-                            errorsFound += ('<li>' + text + '</li>');
-                            console.log("DETECTED: " + text);
-                        }
-                    }
-                }
-                console.log("------------------");
-                errorsFound += '</ol>';
-
-                // if (errorsFound.length > 0) {
-                //     res.write(errorsFound);
-                // } else {
-                //     res.write('<p> No errors were found </p>');
-                // }
-
-
-                var fileName = timePrefix + "sortedClients.xlsx";
-                var filePath = __dirname + "\\sortedContacts\\" + fileName;
-                let buffer = xlsx.build([{ name: "Sorted_Clients_Sheet", data: phoneData }]);
+                //3. Download
+                let fileName = timePrefix + "sortedClients.xlsx";
+                let filePath = __dirname + "\\sortedContacts\\" + fileName;
+                let buffer = xlsx.build([{ name: "Sorted_Clients_Sheet", data: filteredClients }]);
 
                 fs.writeFile(filePath, buffer, 'utf8', function(err) {
                     if (err) console.log(err);
-                    console.log("sortedClients.xlsx was saved in the current directory!");
-                    return res.download(filePath, fileName);
+                    console.log(fileName + " was saved in the current directory!");
+                    res.download(filePath, fileName);
+                    res.render('filterapp', {
+                        title: 'Filter Clients',
+                        firstTime: false,
+                        foundErrors: logger.foundErrors,
+                        responseMessage: logger.responseMessage
+                    });
                 });
-
-                // //res.download(filePath, fileName);
-                // //return res.end();
-                // //return;
             });
         });
     });
 });
 
+
+app.get('/invalidFile', (req, res, next) => {
+    res.render('problemOccured', {
+        description: 'Invalid file has been selected',
+        title: 'Incorrect file format'
+    });
+});
+
 app.use((req, res, next) => {
-    res.status(404).render('404');
+    res.status(404).render('problemOccured', { description: 'Ups, page not found. Check your URL', title: 'Page not found' });
 });
 
 app.listen(3000);
--- util/formatting.js
+++ util/formatting.js
@@ -1,6 +1,7 @@
 const xlsx = require('node-xlsx');
+const regex = require("regex");
 
-var formating = {
+let formatting = {
     transformName: function(nominative) {
         if (nominative.endsWith("as")) {
             return nominative.substring(0, nominative.lastIndexOf("as")) + "ai";
@@ -20,9 +21,18 @@ var formating = {
         return nominative;
     },
 
+    transformEntireName: function(fullname) {
+        let names = fullname.split(" ");
+        let returnName = "";
+        names.forEach(name => {
+            returnName += formatting.transformName(name) + " ";
+        });
+        return returnName;
+    },
+
     getTimePrefix: function() {
-        var d = new Date();
-        var timePrefix = ("\\" +
+        let d = new Date();
+        let timePrefix = ("\\" +
             d.getFullYear() + "-" +
             ("00" + (d.getMonth() + 1)).slice(-2) + "-" +
             ("00" + d.getDate()).slice(-2) + "-[" +
@@ -33,46 +43,171 @@ var formating = {
         return timePrefix;
     },
 
-    getUnwantedPhoneNumbers: function(unwantedPhoneNumbersFilePath) {
-        var obj = xlsx.parse(unwantedPhoneNumbersFilePath); // parses a file
-        var rows = [];
-
+    getAllClients: function(clientsFilePath) {
+        let clientsXlsx = xlsx.parse(clientsFilePath);
+        const allClients = [];
         //looping through all sheets
-        for (var i = 0; i < obj.length; i++) {
-            var sheet = obj[i];
+        for (let i = 0; i < clientsXlsx.length; i++) {
+            let sheet = clientsXlsx[i];
             //loop through all rows in the sheet
-            for (var j = 0; j < sheet['data'].length; j++) {
+            for (let j = 0; j < sheet['data'].length; j++) {
                 //add the row to the rows array
-                //rows.push(sheet['data'][j][0]);
-
-                let rawNumber = String(sheet['data'][j][0]);
-                let baseNumber = rawNumber;
-
-                //console.log("rawNumber " + rawNumber);
-
-                if (rawNumber.startsWith("86")) {
-                    baseNumber = rawNumber.substring(2);
-                } else if (rawNumber.startsWith("3706")) {
-                    baseNumber = rawNumber.substring(4);
-                } else if (rawNumber.startsWith("+3706")) {
-                    baseNumber = rawNumber.substring(5);
+                if (sheet['data'][j].length >= 2) {
+                    const client = {};
+                    client.name = String(sheet['data'][j][0]);
+                    client.nameTransformed = formatting.transformEntireName(String(sheet['data'][j][0]));
+                    client.phoneNumberOriginal = String(sheet['data'][j][1]);
+                    client.phoneNumberNormalized = String(formatting.normalizePhoneNumber(sheet['data'][j][1]));
+                    console.log("CLIENT INFO:", client.name, client.nameTransformed, client.phoneNumberOriginal, client.phoneNumberNormalized);
+                    allClients.push(client);
                 }
-
-                // console.log("baseNumber: " + baseNumber);
-                // console.log(baseNumber);
-                // console.log("8" + baseNumber);
-                // console.log("370" + baseNumber);
-                // console.log("+370" + baseNumber);
-
-                rows.push(String(baseNumber));
-                rows.push(String("86" + baseNumber));
-                rows.push(String("3706" + baseNumber));
-                rows.push(String("+3706" + baseNumber));
             }
         }
-        //console.log(rows);
-        return rows;
+        return allClients;
+    },
+
+    getUnwantedPhoneNumbers: function(unwantedPhoneNumbersFilePath) {
+        let obj = xlsx.parse(unwantedPhoneNumbersFilePath);
+        let phoneNumbers = [];
+
+        for (let i = 0; i < obj.length; i++) {
+            let sheet = obj[i];
+            //loop through all rows in the sheet
+            for (let j = 0; j < sheet['data'].length; j++) {
+                if (sheet['data'][j].length >= 1) {
+                    phoneNumber = [];
+                    phoneNumber.phoneNumberOriginal = String(sheet['data'][j][0]);
+                    phoneNumber.phoneNumberNormalized = String(formatting.normalizePhoneNumber(sheet['data'][j][0]))
+                    phoneNumbers.push(phoneNumber);
+                }
+            }
+        }
+        return phoneNumbers;
+    },
+
+    normalizePhoneNumber: function(phoneNumber) {
+        let trimmedPhoneNumber = String(phoneNumber).replace(/[^0-9]/g, '');
+        return formatting.removePrefix(trimmedPhoneNumber);
+    },
+
+    removePrefix: function(phoneNumber) {
+        let phoneNumberStr = String(phoneNumber);
+        return phoneNumberStr.replace(/^(00)?(33|370|8)/, "");
+    },
+
+    isThisPhoneNumberUnwanted: function(client, unwantedPhoneNumbers) {
+        let answer = false;
+        if ("" == client.phoneNumberNormalized) answer = true;
+        else {
+            //console.log();
+            unwantedPhoneNumbers.forEach((badphoneNumber) => {
+                //console.log("COMPARING: " + client.phoneNumberNormalized + " and " + badphoneNumber.phoneNumberNormalized);
+                if (client.phoneNumberNormalized == badphoneNumber.phoneNumberNormalized) {
+                    //console.log("MISMATCH: " + client.phoneNumberNormalized + " and " + badphoneNumber.phoneNumberNormalized);
+                    answer = true;
+                }
+            });
+        }
+        return answer;
+    },
+
+    filterContacts: function(allClients, unWantedPhoneNumbers, logger) {
+        let filteredClients = [];
+        allClients.forEach(client => {
+            if (!formatting.isThisPhoneNumberUnwanted(client, unWantedPhoneNumbers)) {
+                let newGoodClient = [client.name, client.nameTransformed, client.phoneNumberOriginal];
+                filteredClients.push(newGoodClient);
+            } else {
+                logger.foundErrors = true;
+                const text = "DETECTED: " + client.name + " " + client.nameTransformed + " " + client.phoneNumberOriginal;
+                console.log(text);
+                logger.responseMessage.push(text);
+            }
+        });
+        return filteredClients;
+    },
+
+    test_removePrefix: function() {
+        console.log("------------------------------------");
+        console.log("Testing removePrefix");
+        const testCases = [
+            ['867883756', '67883756'],
+            ['37067883756', '67883756'],
+            ['00867883756', '67883756'],
+            ['3367883756', '67883756'],
+            ['003367883756', '67883756']
+        ];
+
+        testCases.forEach((row) => {
+            if (formatting.normalizePhoneNumber(row[0]) != row[1]) {
+                console.log("TEST FAILED: expected" + formatting.normalizePhoneNumber(row[0]) + " vs " + row[1]);
+            }
+        });
+        console.log("------------------------------------");
+    },
+
+    test_normalizePhoneNumber: function() {
+        console.log("------------------------------------");
+        console.log("Testing normalizePhoneNumber");
+        const testCases = [
+            ['86788 3756', '67883756'],
+            ['+37067  883756', '67883756'],
+            [' 867883756', '67883756'],
+            ['(370) 678 83756', '67883756'],
+            ['8678837 56', '67883756'],
+            ['370.678.83756 ', '67883756'],
+            ['+370-67-883756 ', '67883756'],
+            ['3301234567', '01234567']
+        ];
+
+        testCases.forEach((row) => {
+            if (formatting.normalizePhoneNumber(row[0]) != row[1]) {
+                console.log("TEST FAILED: " + row[0] + " ->" + formatting.normalizePhoneNumber(row[0]) + " vs " + row[1]);
+            }
+        });
+        console.log("------------------------------------");
+    },
+
+    test_isThisPhoneNumberUnwanted: function() {
+        console.log("------------------------------------");
+        console.log("Testing isThisPhoneNumberUnwanted");
+        const unwantedPhoneNumbers = ['861234567', '37067883756', '33 01 23 45 67'];
+
+        let normalizedUnwantedPhoneNumbers = [];
+        unwantedPhoneNumbers.forEach(phoneNumber => {
+            let normalizedPhoneNumber = formatting.normalizePhoneNumber(phoneNumber);
+            console.log("normalizedPhoneNumber: " + normalizedPhoneNumber);
+            normalizedUnwantedPhoneNumbers.push(normalizedPhoneNumber);
+        });
+
+        console.log(normalizedUnwantedPhoneNumbers);
+
+        const testCases = [
+            ['861234567', true],
+            ['861111111', false],
+            ['37067883756', true],
+            ['37067888888', false],
+            ['cat', true],
+            ['3301234567', true],
+            ['123456789', false]
+        ];
+
+        testCases.forEach((test) => {
+            console.log("WORKING with:" + test);
+            let normalizedTest = formatting.normalizePhoneNumber(test[0]);
+            if (formatting.isThisPhoneNumberUnwanted(normalizedTest, normalizedUnwantedPhoneNumbers) != test[1]) {
+                console.log("TEST FAILED: " + test[0] + " -> " + formatting.isThisPhoneNumberUnwanted(normalizedTest, normalizedUnwantedPhoneNumbers) + " vs " + test[1]);
+                console.log("NORMALIZED: " + normalizedTest);
+            }
+        });
+        console.log("------------------------------------");
+    },
+
+    runAllTests: function() {
+        formatting.test_removePrefix();
+        formatting.test_normalizePhoneNumber();
+        //formatting.test_isThisPhoneNumberUnwanted();
     }
 };
 
-module.exports = formating;
+module.exports = formatting;
